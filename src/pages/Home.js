137b3fde9ad34d37d4a317231ec8d8de53aa5edc
Alex Sherman <asherman.ca@gmail.com>
2022-06-05 19:20:13 -0700

    semi functional

---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+const Home = () => {
+	return (
+		<div className='home-container'>
+			<div className='child'>
+				<div className='accord'>
+					<input type='checkbox' defaultChecked />
+					<i></i>
+					<h2>Words Title</h2>
+					<p>
+						lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
+						ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+						lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
+
+{
+	/* <div>
+<input type='checkbox' name='my-checkbox' id='opt-in' />
+<label for='opt-in'>Check me!</label>
+</div> */
+}

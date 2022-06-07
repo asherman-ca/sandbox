c68e1f1922e7dc883f3e66c96c7c53162ff258b3
Alex Sherman <asherman.ca@gmail.com>
2022-06-07 12:25:34 -0700

    cleanup

--- src/components/SlideButton.jsx
+++ src/components/SlideButton.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 const SlideButton = ({ children }) => {
 	return (
-		<div className='button-container'>
-			<div className='button'>
-				{children}
-				<div className='button-bg'>
-					<div>{children}</div>
-				</div>
+		<div className='button'>
+			{children}
+			<div className='button-bg'>
+				<div>{children}</div>
 			</div>
 		</div>
 	);

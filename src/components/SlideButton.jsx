e05feb7bccdcf695cf665b05fab5a1d99cce8643
Alex Sherman <asherman.ca@gmail.com>
2022-06-07 12:03:33 -0700

    button border container

--- src/components/SlideButton.jsx
+++ src/components/SlideButton.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const SlideButton = ({ children }) => {
 	return (
-		<div className='button'>
-			{children}
-			<div className='button-bg'>
-				<div>{children}</div>
+		<div className='button-container'>
+			<div className='button'>
+				{children}
+				<div className='button-bg'>
+					<div>{children}</div>
+				</div>
 			</div>
 		</div>
 	);
